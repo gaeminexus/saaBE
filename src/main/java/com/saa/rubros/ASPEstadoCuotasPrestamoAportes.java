3e5be4cbf8fbbaa7fa20dcfd24b652d103b281ec
xeonpotato <xeonpotato@gmail.com>
2025-12-11 13:01:01 -0500

    Rubro de estado de cuotas

---
+++ src/main/java/com/saa/rubros/ASPEstadoCuotasPrestamoAportes.java
@@ -0,0 +1,15 @@
+package com.saa.rubros;
+
+/**
+ * Rubro de estado de carga de archivo de Petroamazonas.(170)
+ */
+public interface ASPEstadoCuotasPrestamoAportes {
+	
+	// Ids de los elementos hijos
+	public static final int RAIZ = 0;
+	public static final int PENDIENTE = 1;
+	public static final int PAGADA = 2;
+	public static final int EN_MORA = 3;
+	public static final int ANULADA = 4;
+
+}
