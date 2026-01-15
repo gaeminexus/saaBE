e660f3d8d1e16c351e5e245f5af045e518c98ebf
GnRubenB <nexusrubenbenso@gmail.com>
2026-01-15 18:07:26 -0500

    Tabla Saldos of Contabilidad

---
+++ src/main/java/com/saa/ejb/contabilidad/service/SaldosService.java
@@ -0,0 +1,45 @@
+package com.saa.ejb.contabilidad.service;
+
+import java.util.List;
+
+import com.saa.basico.util.DatosBusqueda;
+import com.saa.model.contabilidad.Saldos;
+
+import jakarta.ejb.Local;
+
+@Local
+public interface SaldosService{
+	
+	
+	/**
+	 * Método para verificar si se trata de una cuenta de primer nivel
+	 * @param cuenta		: Saldos a verificar
+	 * @return				: Variable que indica si es que si o no
+	 * @throws Throwable	: Excepcion
+	 */
+	 Boolean verificaPrimerNivel(String cuenta) throws Throwable;
+	
+	/**
+	 * Reversa el contenido de una cadena de strings
+	 * @param cadena 		: Cadena a reversar
+	 * @return		 		: Cadena reversada
+	 * @throws Throwable	: Excepcion
+	 */
+	 String reversaCaracteres(String cadena) throws Throwable;
+	
+	/**
+	 * Obtiene el id del padre mediante la cuenta
+	 * @param cuenta		: Saldos para buscar id del padre
+	 * @return				: Id de la cuenta padre
+	 * @throws Throwable	: Excepcion
+	 */
+	 String obtieneSaldosPadre(String cuenta) throws Throwable;
+
+	Saldos saveSingle(Saldos registro);
+
+	Object selectByCriteria(List<DatosBusqueda> registros);
+
+	Object selectByCriteria1(List<DatosBusqueda> registros);
+	 
+
+}
---
+++ src/main/java/com/saa/ws/rest/contabilidad/SaldosRest.java
@@ -0,0 +1,106 @@
+package com.saa.ws.rest.contabilidad;
+
+import java.util.List;
+
+import com.saa.basico.util.DatosBusqueda;
+import com.saa.ejb.contabilidad.dao.SaldosDaoService;
+import com.saa.ejb.contabilidad.service.SaldosService;
+import com.saa.model.contabilidad.NombreEntidadesContabilidad;
+import com.saa.model.contabilidad.Saldos;
+
+import jakarta.ejb.EJB;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriInfo;
+
+@Path("slds")
+public class SaldosRest {
+
+    @EJB
+    private SaldosDaoService saldosDaoService;
+
+    @EJB
+    private SaldosService saldosService;
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Default constructor.
+     */
+    public SaldosRest() {
+    }
+
+    /**
+     * Retrieves representation of an instance of SaldosRest
+     * 
+     * @return an instance of String
+     * @throws Throwable
+     */
+    @GET
+    @Path("/getAll")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAll() {
+        try {
+            List<Saldos> lista = saldosDaoService.selectAll(NombreEntidadesContabilidad.SALDOS);
+            return Response.status(Response.Status.OK).entity(lista).type(MediaType.APPLICATION_JSON).build();
+        } catch (Throwable e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Error al obtener saldoss: " + e.getMessage()).type(MediaType.APPLICATION_JSON).build();
+        }
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/getId/{id}")
+    public Response getId(@PathParam("id") Long id) {
+        try {
+            Saldos saldos = saldosDaoService.selectById(id, NombreEntidadesContabilidad.SALDOS);
+            if (saldos == null) {
+                return Response.status(Response.Status.NOT_FOUND).entity("Saldos con ID " + id + " no encontrado").type(MediaType.APPLICATION_JSON).build();
+            }
+            return Response.status(Response.Status.OK).entity(saldos).type(MediaType.APPLICATION_JSON).build();
+        } catch (Throwable e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Error al obtener saldos: " + e.getMessage()).type(MediaType.APPLICATION_JSON).build();
+        }
+    }
+
+
+    @POST
+    @Path("selectByCriteria")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response selectByCriteria(List<DatosBusqueda> registros) {
+        System.out.println("selectByCriteria de SALDOS");
+        try {
+            return Response.status(Response.Status.OK)
+                    .type(MediaType.APPLICATION_JSON).build();
+        } catch (Throwable e) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity(e.getMessage())
+                    .type(MediaType.APPLICATION_JSON).build();
+        }
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response delete(@PathParam("id") Long id) {
+        System.out.println("LLEGA AL SERVICIO DELETE - SALDOS");
+        try {
+            Saldos elimina = new Saldos();
+            saldosDaoService.remove(elimina, id);
+            return Response.status(Response.Status.NO_CONTENT).build();
+        } catch (Throwable e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Error al eliminar saldos: " + e.getMessage()).type(MediaType.APPLICATION_JSON).build();
+        }
+    }
+
+}
